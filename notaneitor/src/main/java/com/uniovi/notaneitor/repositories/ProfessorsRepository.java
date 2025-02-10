0d21a9d5b1fb71cbe4aaf984bb471359a9ffe1d9
uo294197 <uo294197@uniovi.es>
2025-02-10 10:23:04 +0100

    SDI-2425-914-sesión3-ejercio-complementario2.

---
+++ notaneitor/src/main/java/com/uniovi/notaneitor/repositories/ProfessorsRepository.java
@@ -0,0 +1,8 @@
+package com.uniovi.notaneitor.repositories;
+
+import com.uniovi.notaneitor.entities.Mark;
+import com.uniovi.notaneitor.entities.Professor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfessorsRepository extends CrudRepository<Professor, Long> {
+}
