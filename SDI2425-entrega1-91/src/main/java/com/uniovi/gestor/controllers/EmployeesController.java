b7b5e65e2f24330b86ef8f149f67c96aaefc73d8
pelayosl <uo294217@uniovi.es>
2025-03-14 17:55:27 +0100

    [LOG] EmployeesController - peticiones y alta

--- SDI2425-entrega1-91/src/main/java/com/uniovi/gestor/controllers/EmployeesController.java
+++ SDI2425-entrega1-91/src/main/java/com/uniovi/gestor/controllers/EmployeesController.java
@@ -1,6 +1,7 @@
 package com.uniovi.gestor.controllers;
 import com.uniovi.gestor.PasswordGenerator;
 import com.uniovi.gestor.services.EmployeesService;
+import com.uniovi.gestor.services.LogService;
 import com.uniovi.gestor.services.RolesService;
 import com.uniovi.gestor.services.SecurityService;
 import com.uniovi.gestor.validators.AddEmployeeFormValidator;
@@ -27,15 +28,17 @@ import java.security.Principal;
 public class EmployeesController {
     private final EmployeesService employeesService;
     private final SecurityService securityService;
+    private final LogService logService;
     private final AddEmployeeFormValidator addEmployeeFormValidator;
     private final EditEmployeeFormValidator editEmployeeFormValidator;
     private final RolesService rolesService;
     private final ChangePasswordValidator changePasswordValidator;
 
-    public EmployeesController(EmployeesService employeesService, SecurityService securityService, AddEmployeeFormValidator
+    public EmployeesController(EmployeesService employeesService, SecurityService securityService, LogService logService, AddEmployeeFormValidator
             addEmployeeFormValidator, EditEmployeeFormValidator editEmployeeFormValidator , RolesService rolesService, ChangePasswordValidator changePasswordValidator) {
         this.employeesService = employeesService;
         this.securityService = securityService;
+        this.logService = logService;
         this.addEmployeeFormValidator = addEmployeeFormValidator;
         this.rolesService = rolesService;
         this.editEmployeeFormValidator = editEmployeeFormValidator;
@@ -43,7 +46,7 @@ public class EmployeesController {
     }
     @RequestMapping("/employee/list")
     public String getListado(Model model, Pageable pageable, HttpSession session) {
-
+        logService.log("PET", "PET [GET] /employee/list");
         Page<Employee> employees = employeesService.getEmployees(pageable);
 
         model.addAttribute("employeesList", employees.getContent());
@@ -57,12 +60,18 @@ public class EmployeesController {
     }
     @RequestMapping(value = "/employee/add")
     public String getEmployee(Model model) {
+        logService.log("PET", "PET [GET] /employee/add");
         model.addAttribute("employee", new Employee());
         return "employee/add";
     }
 
     @RequestMapping(value = "/employee/add", method = RequestMethod.POST)
     public String signup(@Validated Employee employee, BindingResult result, Model model, HttpSession session) {
+        String logMessage = String.format(
+                "PET [POST] /employee/add | parameters: DNI=%s, Nombre=%s, Email=%s",
+                employee.getDni(), employee.getName(), employee.getLastName()
+        );
+        logService.log("PET", logMessage);
         addEmployeeFormValidator.validate(employee, result);
         model.addAttribute("employee", employee);
         if (result.hasErrors()) {
@@ -74,20 +83,30 @@ public class EmployeesController {
         employee.setRole(rolesService.getRoles()[0]);
         employeesService.addEmployee(employee);
         session.setAttribute("generatedPassword", generatedPassword);
+
+        String logMessage2 = String.format(
+                "ALTA [POST] /employee/add | parameters: DNI=%s, Nombre=%s, Email=%s",
+                employee.getDni(), employee.getName(), employee.getLastName()
+        );
+        logService.log("ALTA", logMessage2);
+
         return "redirect:/employee/list";
     }
     @RequestMapping("/employee/details/{id}")
     public String getDetail(Model model, @PathVariable Long id) {
+        logService.log("PET", "PET [GET] /employee/details/" + id);
         model.addAttribute("employee", employeesService.getEmployee(id));
         return "employee/details";
     }
     @RequestMapping("/employee/delete/{id}")
     public String delete(@PathVariable Long id) {
+        logService.log("PET", "PET [GET] /employee/delete/" + id);
         employeesService.deleteEmployee(id);
         return "redirect:/employee/list";
     }
     @RequestMapping(value = "/employee/edit/{id}")
     public String getEdit(Model model, @PathVariable Long id) {
+        logService.log("PET", "PET [GET] /employee/edit/" + id);
         Employee employee = employeesService.getEmployee(id);
         model.addAttribute("employee", employee);
         return "employee/edit";
@@ -95,8 +114,8 @@ public class EmployeesController {
 
     @RequestMapping(value = "/employee/edit/{id}", method = RequestMethod.POST)
     public String setEdit(@Validated Employee employee, BindingResult result, @PathVariable Long id) {
+        logService.log("PET", "PET [POST] /employee/edit/" + id);
         editEmployeeFormValidator.validate(employee, result);
-
         if (result.hasErrors()) {
             return "employee/edit";
         }
@@ -112,6 +131,7 @@ public class EmployeesController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, @RequestParam(value = "logout", required = false) String logout, HttpServletRequest request) {
+        logService.log("PET", "PET [GET] /login");
         if (logout != null) {
             Object logoutAttribute = request.getSession().getAttribute("SPRING_SECURITY_LOGOUT");
 
@@ -125,6 +145,7 @@ public class EmployeesController {
 
     @RequestMapping(value = "/login/success", method = RequestMethod.GET)
     public String loginSuccess() {
+        logService.log("PET", "PET [GET] /login/success");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String dni = auth.getName();
         Employee activeEmployee = employeesService.getEmployeeByDni(dni);
@@ -136,12 +157,14 @@ public class EmployeesController {
 
     @RequestMapping(value = "/login/error", method = RequestMethod.GET)
     public String loginError(Model model) {
+        logService.log("PET", "PET [GET] /login/error");
         model.addAttribute("error", true);
         return "login";
     }
 
     @RequestMapping(value = {"/home"}, method = RequestMethod.GET)
     public String home(Model model) {
+        logService.log("PET", "PET [GET] /home");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String dni = auth.getName();
         Employee activeEmployee = employeesService.getEmployeeByDni(dni);
@@ -149,27 +172,24 @@ public class EmployeesController {
         return "home";
     }
 
-
-
-
-
-
-
     @RequestMapping("/employee/list/update")
     public String updateList(Model model, Pageable pageable) {
+        logService.log("PET", "PET [GET] /employee/list/update");
         model.addAttribute("employeesList", employeesService.getEmployees(pageable));
         return "employee/list :: employeeTable";
     }
 
     @RequestMapping("/employee/changePassword")
     public String getChangePassword(Model model, Principal principal) {
+        logService.log("PET", "PET [GET] /employee/changePassword | parameters: DNI=" + principal.getName());
         model.addAttribute("employee", new Employee());
         return "employee/changePassword";
     }
 
     @RequestMapping(value = "/employee/changePassword", method = RequestMethod.POST)
     public String setChangePassword(@Validated Employee employee, BindingResult result) {
-
+        logService.log("PET", "PET [POST] /employee/changePassword | " +
+                "parameters: DNI=" + employee.getDni() + ", result=" + result.toString());
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         employee.setDni(auth.getName());
         changePasswordValidator.validate(employee, result);
@@ -177,17 +197,9 @@ public class EmployeesController {
         if(result.hasErrors()) {
             return "employee/changePassword";
         }
-
-
-
         Employee activeEmployee = employeesService.getEmployeeByDni(auth.getName());
-
         activeEmployee.setPassword(employee.getNewPassword());
-
         employeesService.addEmployee(activeEmployee);
-
         return "home";
     }
-
-
 }
