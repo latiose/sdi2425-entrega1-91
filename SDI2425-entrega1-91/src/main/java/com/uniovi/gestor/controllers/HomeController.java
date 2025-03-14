1107fbab587262444a671dd247dcf67584fae374
pelayosl <uo294217@uniovi.es>
2025-03-14 18:21:43 +0100

    [LOG] HomeController

--- SDI2425-entrega1-91/src/main/java/com/uniovi/gestor/controllers/HomeController.java
+++ SDI2425-entrega1-91/src/main/java/com/uniovi/gestor/controllers/HomeController.java
@@ -1,13 +1,21 @@
 package com.uniovi.gestor.controllers;
 
+import com.uniovi.gestor.services.LogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    private final LogService logService;
+
+    public HomeController(LogService logService) {
+        this.logService = logService;
+    }
+
     @RequestMapping("/")
     public String index() {
+        logService.log("PET", "PET [GET] /");
         return "index";
     }
 
